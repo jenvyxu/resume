9b309dc6db256ea1ab1dbe4b9ad68e7f71cf1d52
xzw <1272117264@qq.com>
2018-07-02 22:46:06 +0800

    用MVC思想优化代码

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,14 +1,31 @@
 !function(){
-    let mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
+    var view=document.querySelector('#mySlides')
+    var controller={
+        view:null,
+        swiper:null,
+        swiperOptions:{
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
         },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    })
+        init:function(view){
+                this.view=view
+                this.initSwiper()
+            },
+        initSwiper:function(){
+                this.swiper= new Swiper (
+                    this.view.querySelector('.swiper-container'),
+                    this.swiperOptions )
+            }
+
+        }
+
+controller.init(view)
 }.call()
 
 
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,36 +1,28 @@
 !function(){
-    let liTags=document.querySelectorAll('nav.menu > ul >li')
-    for(let i=0;i<liTags.length;i++){
-        liTags[i].onmouseenter=function(x){
-            x.currentTarget.classList.add('active')
-        }
-        liTags[i].onmouseleave=function(x){
-            x.currentTarget.classList.remove('active')
-        }
-    }
-
-    let aTags=document.querySelectorAll('nav.menu > ul > li > a')
-
-    function animate(time) {
-        requestAnimationFrame(animate);
-        TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
-
-    for(let i=0;i<aTags.length;i++){
-        aTags[i].onclick=function(x){
-            x.preventDefault()
-            let a=x.currentTarget;
-            let href=a.getAttribute('href')
-            let element=document.querySelectorAll(href)
+    var view=document.querySelector('nav.menu')
+    var controller={
+        view:null,
+        aTags:null,
+        liTags:null,
+        init:function(view){
+            this.view=view
+            this.initAnimation()
+            this.bindEvents()
+        },
+        initAnimation:function(){
+            function animate(time) {
+                requestAnimationFrame(animate);
+                TWEEN.update(time);
+            }
+            requestAnimationFrame(animate)
+        },
+        scrollToElement:function(element){
             let top=element[0].offsetTop  //element[0]才是一个div
-
             let currentTop=window.scrollY
             let targetTop=top-80
             let s=targetTop-currentTop
             let t=Math.abs((s/100)*300)
             if(t>500){t=500}
-
             let coords = {y: currentTop }
             let tween = new TWEEN.Tween(coords)
                 .to({y: targetTop }, t)
@@ -39,8 +31,31 @@
                     window.scrollTo(0,coords.y)
                 })
                 .start();
-        }
-    }
+        },
 
+        bindEvents:function(){
+            let aTags=view.querySelectorAll('nav.menu > ul > li > a')
+            let liTags=view.querySelectorAll('nav.menu>ul>li')
+            for(let i=0;i<aTags.length;i++){
+                aTags[i].onclick=(x)=>{
+                    x.preventDefault()
+                    let a=x.currentTarget;
+                    let href=a.getAttribute('href')
+                    let element=document.querySelectorAll(href)
+                    this.scrollToElement(element)
+                }
+            }
+            for(let i=0;i<liTags.length;i++){
+                liTags[i].onmouseenter=function(x){
+                    x.currentTarget.classList.add('active')
+                }
+                liTags[i].onmouseleave=function(x){
+                    x.currentTarget.classList.remove('active')
+                }
+            }
+        },
+    }
+    controller.init(view)
+    console.log(1)
 }.call()
 
