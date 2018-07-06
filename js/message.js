aa41f775374d8eed14934f959c00c0f0a05cd2f2
xzw <1272117264@qq.com>
2018-07-06 09:32:53 +0800

    修改网站的整体CSS样式

--- js/message.js
+++ js/message.js
@@ -1,6 +1,5 @@
 !function(){
     var view=document.querySelector('section.message')
-
     var model={
         init:function(){
             //初始化
@@ -14,12 +13,13 @@
             return query.find()
         },
         //创建数据并保存
-        save:function(name,content){
+        save:function(name,content,time){
             let Message = AV.Object.extend('Message');
             let message= new Message();
             return message.save({  //promise对象
                 'name':name,
-                'content': content
+                'content': content,
+                'time':time
             })
         }
     }
@@ -32,6 +32,7 @@
             this.view=view
             this.model=model
             this.messageList=view.querySelector('#messageList')
+            console.log(this.messageList)
             this.form=view.querySelector('form')
             this.model.init()
             this.loadMessages()
@@ -41,12 +42,26 @@
             var query = new AV.Query('Message');
             this.model.fetch().then(
                     (messages)=>{
+                        console.log(messages)
                         let array=messages.map((item)=>item.attributes)
+                        console.log(array
+                    )
                         array.forEach((item)=>{
                             let li=document.createElement('li')
-                            li.innerText=`${item.name}:${item.content}`
+                            let string=` 
+                                <div id="guest"><img src="./img/guest.jpg" alt=""></div>
+                                     <div class="nameAndMessage">
+                                            <div class="nameAndTime">
+                                            <div class="name">${item.name}</div>
+                                    <div class="time">${item.time}</div>
+                                    </div>
+                                    <div class="inner-message">${item.content}
+                                </div>
+                            </div>`
+                            li.innerHTML=string
                             this.messageList.appendChild(li)
                         })
+
                     })
         },
         bindEvents:function(){
@@ -57,12 +72,27 @@
         },
         saveMessage:function(){
             let myform=this.form
-            let content=myform.querySelector('input[name=content]').value
+            let content=myform.querySelector('textarea[name=content]').value
             let name=myform.querySelector('input[name=name]').value
-            this.model.save(name,content).then(function(object) {
-                let li=document.createElement('li')
-                li.innerText=`${object.attributes.name}:${object.attributes.content}`
+            let myDate=new Date()
+            let month=Number(myDate.getMonth())+1
+            let time=month+'/'+myDate.getDate()+'/'+myDate.getFullYear()+' ' +myDate.toString().substring(15,24)
+            this.model.save(name,content,time).then(
+                function(object) {
+                let div=document.createElement('div')
                 let messageList=document.querySelector('#messageList')
+                let string=`  
+                   <div id="guest"><img src="./img/guest.jpg" alt=""></div>
+                <div class="nameAndMessage">
+                    <div class="nameAndTime">
+                        <div class="name">${object.attributes.name}</div>
+                        <div class="time">${time}</div>
+                    </div>
+                    <div class="inner-message">${object.attributes.content}
+                    </div>
+                </div>`
+                let li=document.createElement('li')
+                li.innerHTML=string
                 messageList.appendChild(li)
                 myform.querySelector('input[name=content]').value=''
             })
