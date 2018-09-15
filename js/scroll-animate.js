86212409a06c8c525e6d65a3cd8e622e54c8014e
jenvyxu <1272117264@qq.com>
2018-09-15 13:14:46 +0800

    添加滚动特效

---
+++ js/scroll-animate.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('section,.everyWork',{
+    delay: 1000,
+    useDelay: 'onload',
+    reset: true,
+    distance: '150px'
+})
+ScrollReveal().reveal('.skillDescription>li', {
+            reset: true,
+            useDelay:'onload',
+            interval: 400,
+            scale: 0.85
+});
